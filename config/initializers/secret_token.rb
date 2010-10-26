3b6e96dedfdc03e8a41e4745cf77ba1cd57713ba
Sven Tissot <einstein@BIGMACST.local>
2010-10-26 23:12:21 +0200

    wine3 initial on rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine3::Application.config.secret_token = 'f650a0d6e844528cca7bf0939887d59e0c3c71bdcb8bd394716d7dc00f495feff2a6a9c67141989b5c5031a0d87e202d38f0e91b3e46dd9ed18a6be89d9d4b9c'
